ae0af2962214ea64f5616662f4d9d0a2c4dae000
282888888 <61619957+282888888@users.noreply.github.com>
2025-07-09 23:58:01 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -101,7 +101,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["liushan.vip"];
-pub const RS_PUB_KEY: &str = "yXwx8Tm51byuhp7qxWCyMqffDwCJdZP9nhM8WUBaxn8=";
+pub const RS_PUB_KEY: &str = "NzhCnKt6teZlIFt3L6xq33pLPgwAe3fC8lLe3J55f6k=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
